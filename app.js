54274b1e73d37d25e9b0d22d6841fa61eb4036c1
Aroop Ghosh <aroop@netopss-MacBook-Pro-4.local>
2019-04-22 19:23:16 +0530

    .

--- app.js
+++ app.js
@@ -32,9 +32,8 @@ function startApp() {
   }));
   app.use(methodOverride());
   app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'userid, application-id, appid, secret-id, token, Content-Type, Authorization, *');
+    res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
     if ('OPTIONS' === req.method) {
       res.sendStatus(200);
