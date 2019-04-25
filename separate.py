2988947292a903c6b80487c5620a725a6acc570f
aroop <aroop.ghosh@tarento.com>
2019-04-25 12:34:23 +0530

    string length decreased

--- separate.py
+++ separate.py
@@ -6,12 +6,12 @@ import sys
 
 def detect_language(str):
     wordList = re.sub("[^\w]", " ",  str).split()
-    if len(str) < 10 or len(wordList) <6:
+    if len(wordList) == 0 or (len(wordList) ==1 and wordList[0].isdigit()) or len(str) < 10 or len(wordList) <3:
         return 'unknown'
     else:
-        maxchar1 = max(wordList[1])
-        maxchar2 = max(wordList[2])
-        maxchar3 = max(wordList[3])
+        maxchar1 = max(wordList[0])
+        maxchar2 = max(wordList[1])
+        maxchar3 = max(wordList[2])
         if u'\u0900' <= maxchar1 <= u'\u097f' or u'\u0900' <= maxchar2 <= u'\u097f' or u'\u0900' <= maxchar3 <= u'\u097f':
             return 'hindi'
         else:
