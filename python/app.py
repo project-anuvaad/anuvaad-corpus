1fe80c899fe15a7c9fd90f0b6b098a0bd2d21307
aroop <aroop@netopss-MacBook-Pro-5.local>
2019-06-11 14:32:42 +0530

    .

--- python/app.py
+++ python/app.py
@@ -42,7 +42,7 @@ def index():
 @app.route('/multiple', methods=['POST'])
 def upload_file():
     print(mp.cpu_count())
-    pool = mp.Pool(8)
+    pool = mp.Pool(mp.cpu_count())
     basename = str(int(time.time()))
     f = request.files['hindi']
     f_eng = request.files['english']
