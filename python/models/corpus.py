ce66fd9a7f10ca4f615593a2cf1d3929ace0eb73
aroop <aroop.ghosh@tarento.com>
2019-06-19 14:52:12 +0530

    corpus listing added

---
+++ python/models/corpus.py
@@ -0,0 +1,11 @@
+from mongoengine import *
+
+class Corpus(Document):
+    name = StringField(required=True)
+    domain = StringField()
+    created_on = StringField()
+    last_modified = StringField()
+    comment = StringField()
+    author = StringField()
+    no_of_sentences = IntField()
+    status = StringField()
