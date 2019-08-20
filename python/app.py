8702ad2c06e11852321898e59ed999b43cb0783e
aroop <aroop.ghosh@tarento.com>
2019-08-20 14:15:02 +0530

    .

--- python/app.py
+++ python/app.py
@@ -252,8 +252,8 @@ def fetch_sentences():
                 corpus.update(set__status=STATUS_PROCESSING)
             sources.append(sent_dict['source'])
         target_lang = 'en'
-        print(corpus_obj)
-        if 'target_lang' in corpus_dict and corpus_dict['target_lang'] is not None:
+        print(corpus_dict[0]['basename'])
+        if 'target_lang' in corpus_dict[0] and corpus_dict[0]['target_lang'] is not None:
             target_lang = LANGUAGES[corpus_dict['target_lang']]
         translation_list = translatesinglesentence(sources, target_lang)
         index = 0
