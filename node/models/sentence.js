53822caf80821fa6045e7bbf66393d850400bd86
Mayank Morya <moryamayank9@gmail.com>
2019-09-02 23:07:19 +0530

    sentences added

--- node/models/sentence.js
+++ node/models/sentence.js
@@ -3,14 +3,18 @@ var LOG = require('../logger/logger').logger
 var Schema = mongoose.Schema;
 
 var SentenceSchema = new Schema({
-    _id: {type: String},
-}, { strict: false });
+    _id: {
+        type: String
+    },
+}, {
+    strict: false
+});
 var Sentence = mongoose.model('Corpussentence', SentenceSchema);
 
 
-Sentence.saveSentences = function(sentences, cb){
+Sentence.saveSentences = function (sentences, cb) {
     LOG.info(sentences.length)
-    Sentence.collection.insertMany(sentences,function(err,docs){
+    Sentence.collection.insertMany(sentences, function (err, docs) {
         if (err) {
             // TODO: handle error
             return cb(err, null)
@@ -19,6 +23,34 @@ Sentence.saveSentences = function(sentences, cb){
             return cb(null, docs)
         }
     })
+}
+
+Sentence.findWithtagId = function (parallelCorpusId, tagId, cb) {
+    var corpusIds = []
+    if (parallelCorpusId != null) {
+        corpusIds = [parallelCorpusId]
+        Sentence.collection.findOne({
+            "tags": tagId,
+            "parallelcorpusid": parallelCorpusId}, function (err, sentences) {
+            if (err) {
+                LOG.error("Unable to find sentences id  due to [%s]", tagId, JSON.stringify(err));
+                return cb(err, null);
+            }
+            console.log("sentences are  "+sentences.sentence)
+            return cb(null, sentences)
+        })
+    } else {
+        
+        Sentence.collection.find({"tags": tagId,"parallelcorpusid": corpusIds}).sort({"index": 1}, function (err, sentences) {
+            if (err) {
+                LOG.error("Unable to find sentences id  due to [%s]", tagId, JSON.stringify(err));
+                return cb(err, null);
+            }
+            console.log("sentences are "+sentences.sentence)
+            return cb(null, sentences)
+        })
+
+    }
 }
 
 module.exports = Sentence;
