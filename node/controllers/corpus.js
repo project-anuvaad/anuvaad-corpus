fe99dd3b70f9d7b9ba191ae2bc604470fe26298b
aroop <aroop@netopss-MacBook-Pro-5.local>
2019-09-10 14:13:33 +0530

    log added

--- node/controllers/corpus.js
+++ node/controllers/corpus.js
@@ -61,7 +61,9 @@ exports.fetchCorpusSentences = function (req, res) {
             if (sentences && Array.isArray(sentences) && sentences.length > 0) {
                 let sentences_arr = []
                 let target_lang = 'en'
+                LOG.info(corpus['target_lang'])
                 target_lang = LANGUAGES[corpus['target_lang']] ? LANGUAGES[corpus['target_lang']] : 'en'
+                LOG.info(target_lang)
                 sentences.map((sentence) => {
                     sentences_arr.push(sentence._doc.source)
                 })
