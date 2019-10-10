6edd342d782ce45cd193388f01b71480492f0834
aroop <aroop@netopss-MacBook-Pro-5.local>
2019-10-10 14:04:10 +0530

    save and fetch benchmark added

---
+++ node/routes/benchmark/benchmark.route.js
@@ -0,0 +1,26 @@
+/*
+ * @Author: ghost 
+ * @Date: 2019-09-01 10:59:10 
+ * @Last Modified by: aroop.ghosh@tarento.com
+ * @Last Modified time: 2019-09-01 10:18:27
+ */
+var benchmarkController = require('../../controllers/benchmark');
+
+
+module.exports = function (router) {
+    router.route('/fetch-benchmarks')
+        .get(benchmarkController.fetchBenchmark);
+
+    // router.route('/fetch-corpus-sentences')
+    //     .get(corpusController.fetchCorpusSentences);
+
+    // router.route('/update-sentences')
+    //     .post(corpusController.updateSentences);
+
+    // router.route('/update-sentences-status')
+    //     .post(corpusController.updateSentencesStatus);
+
+    // router.route('/update-sentences-grade')
+    //     .post(corpusController.updateSentencesGrade);
+
+}
