2f8026d7301fbe9d59df6ab20cc05781ec66aa28
aroop <aroop@netopss-MacBook-Pro-5.local>
2019-10-14 14:44:01 +0530

    toatl grade added

--- node/models/response.js
+++ node/models/response.js
@@ -9,10 +9,11 @@ function Response(statuscode, data) {
     this.data = data;
 }
 
-function Response(statuscode, data, count) {
+function Response(statuscode, data, count, sum) {
     this.statuscode = statuscode;
     this.data = data;
     this.count = count;
+    this.sum = sum
 }
 
 Response.prototype.getRsp = function () {
@@ -22,6 +23,9 @@ Response.prototype.getRsp = function () {
     }
     if(this.count){
         result.count = this.count
+    }
+    if(this.sum){
+        result.sum = this.sum
     }
     return result
 };
