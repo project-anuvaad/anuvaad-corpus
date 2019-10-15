1e3a9740bbcca4fc4a80c2686ef261744f68df64
aroop <aroop@netopss-MacBook-Pro-5.local>
2019-10-15 12:48:32 +0530

    update model api change

--- node/controllers/nmt.js
+++ node/controllers/nmt.js
@@ -26,14 +26,44 @@ exports.updateModels = function (req, res) {
         return res.status(apistatus.http.status).json(apistatus);
     }
     let model = req.body.model
-    NMT.updateModel(model, function (err, doc) {
-        if (err) {
-            let apistatus = new APIStatus(StatusCode.ERR_GLOBAL_SYSTEM, COMPONENT).getRspStatus()
-            return res.status(apistatus.http.status).json(apistatus);
-        }
-        let response = new Response(StatusCode.SUCCESS, COMPONENT).getRsp()
-        return res.status(response.http.status).json(response);
-    })
+    if (model.is_primary) {
+        NMT.findByCondition({ is_primary: true, source_language_code: model.source_language_code, target_language_code: model.target_language_code }, function (err, modeldblist) {
+            if (modeldblist && modeldblist.length > 0) {
+                let modeldb = modeldblist[0]._doc
+                modeldb.is_primary = false
+                NMT.updateModel(modeldb, function (err, doc) {
+                    NMT.updateModel(model, function (err, doc) {
+                        if (err) {
+                            let apistatus = new APIStatus(StatusCode.ERR_GLOBAL_SYSTEM, COMPONENT).getRspStatus()
+                            return res.status(apistatus.http.status).json(apistatus);
+                        }
+                        let response = new Response(StatusCode.SUCCESS, COMPONENT).getRsp()
+                        return res.status(response.http.status).json(response);
+                    })
+                })
+            }
+            else {
+                NMT.updateModel(model, function (err, doc) {
+                    if (err) {
+                        let apistatus = new APIStatus(StatusCode.ERR_GLOBAL_SYSTEM, COMPONENT).getRspStatus()
+                        return res.status(apistatus.http.status).json(apistatus);
+                    }
+                    let response = new Response(StatusCode.SUCCESS, COMPONENT).getRsp()
+                    return res.status(response.http.status).json(response);
+                })
+            }
+        })
+    }
+    else {
+        NMT.updateModel(model, function (err, doc) {
+            if (err) {
+                let apistatus = new APIStatus(StatusCode.ERR_GLOBAL_SYSTEM, COMPONENT).getRspStatus()
+                return res.status(apistatus.http.status).json(apistatus);
+            }
+            let response = new Response(StatusCode.SUCCESS, COMPONENT).getRsp()
+            return res.status(response.http.status).json(response);
+        })
+    }
 }
 
 exports.saveModels = function (req, res) {
