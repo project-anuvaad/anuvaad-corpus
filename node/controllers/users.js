b5485b41c4b9199a100ffbb4bda26b6a0e8f392c
aroop <aroop.ghosh@tarento.com>
2019-10-18 11:44:03 +0530

    list roles added

--- node/controllers/users.js
+++ node/controllers/users.js
@@ -18,6 +18,7 @@ var axios = require('axios');
 var COMPONENT = "users";
 const ES_SERVER_URL = process.env.GATEWAY_URL ? process.env.GATEWAY_URL : 'http://nlp-nmt-160078446.us-west-2.elb.amazonaws.com/admin/'
 const USERS_REQ_URL = ES_SERVER_URL + 'users?count=1000'
+const SCOPE_URL = ES_SERVER_URL + 'scopes?count=1000'
 const PROFILE_BASE_URL = process.env.PROFILE_APP_URL ? process.env.PROFILE_APP_URL : 'http://nlp-nmt-160078446.us-west-2.elb.amazonaws.com/'
 const PROFILE_REQ_URL = PROFILE_BASE_URL + 'corpus/get-profiles'
 
@@ -54,5 +55,21 @@ exports.listUsers = function (req, res) {
 }
 
 
+exports.listRoles = function (req, res) {
+    axios.get(SCOPE_URL).then((api_res) => {
+        if (api_res.data && api_res.data.scopes && Array.isArray(api_res.data.scopes)) {
+            let response = new Response(StatusCode.SUCCESS, api_res.data.scopes).getRsp()
+            return res.status(response.http.status).json(response);
+        }
+        else {
+            let apistatus = new APIStatus(StatusCode.ERR_GLOBAL_SYSTEM, COMPONENT).getRspStatus()
+            return res.status(apistatus.http.status).json(apistatus);
+        }
+    }).catch((e) => {
+        LOG.info(e)
+        let apistatus = new APIStatus(StatusCode.ERR_GLOBAL_SYSTEM, COMPONENT).getRspStatus()
+        return res.status(apistatus.http.status).json(apistatus);
+    })
+}
 
 
