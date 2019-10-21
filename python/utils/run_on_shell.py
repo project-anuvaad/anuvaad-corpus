c4a7f3d0923d7e1303a34c0e5961f4f435c3af4c
Mayank Morya <moryamayank9@gmail.com>
2019-10-21 12:27:05 +0530

    create user and add aouth2 api added

--- python/utils/run_on_shell.py
+++ python/utils/run_on_shell.py
@@ -1,8 +1,13 @@
 import subprocess
 import json
+import logging
 
 CREATE_BASIC_AUTH_BASE = 'eg credentials create -c '
 CREATE_BASIC_AUTH_PART = ' -t basic-auth -p "password='
+
+CREATE_USER_BASE = 'eg users create -p '
+
+log = logging.getLogger('file')
 
 
 def create_basic_auth_credentials(username, password):
@@ -11,3 +16,60 @@ def create_basic_auth_credentials(username, password):
     (output, err) = p.communicate()
     data = json.loads(output.decode('utf-8'))
     return data
+
+
+def create_user(username, firstname, lastname):
+    try:
+        command = CREATE_USER_BASE + '\'username=' + username + '\' -p \'firstname=' + firstname + '\' -p \'lastname=' + lastname + '\' ';
+        p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+        (output, err) = p.communicate()
+        data = json.loads(output.decode('utf-8'))
+        return data
+    except Exception as e:
+        return None
+
+
+def scope_add(username, scope):
+    try:
+        command = 'eg credential:scopes add -t basic-auth --id ' + username + ' ' + scope
+        p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+        (output, err) = p.communicate()
+        data = json.loads(output.decode('utf-8'))
+        log.info(' scope_add : response for username = ' + username + ', scope = ' + str(data))
+        return data
+    except Exception as e:
+        return None
+
+
+def create_oauth(user_name):
+    try:
+        command = 'eg credentials create -c ' + user_name + '-t oauth2'
+        p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+        (output, err) = p.communicate()
+        data = json.loads(output.decode('utf-8'))
+        return data
+    except Exception as e:
+        return None
+
+
+def get_credential_info(username):
+    profile = get_user_info(username)
+    command = 'eg credentials info - t oauth2 ' + profile['id']
+    try:
+        p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+        (output, err) = p.communicate()
+        data = json.loads(output.decode('utf-8'))
+        return data
+    except Exception as e:
+        return None
+
+
+def get_user_info(username):
+    command = ' eg users info ' + username
+    try:
+        p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+        (output, err) = p.communicate()
+        data = json.loads(output.decode('utf-8'))
+        return data
+    except Exception as e:
+        return None
