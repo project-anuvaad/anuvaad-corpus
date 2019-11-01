4cdddbe03f2ff4b44c5c6da189fcc132285d4ba9
aroop <aroop.ghosh@tarento.com>
2019-11-01 13:50:16 +0530

    log added

--- node/controllers/benchmark.js
+++ node/controllers/benchmark.js
@@ -132,7 +132,7 @@ exports.fetchBenchmarkCompareSentences = function (req, res) {
                 let reverse = false
                 let source_code = 'en'
                 let target_code = 'hi'
-                LOG.info()
+                LOG.info(benchmark[0].source_lang)
                 if (benchmark[0].source_lang === 'Hindi') {
                     source_code = 'hi'
                     target_code = 'en'
