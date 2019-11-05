d74a31adbbe4326bda94406a086d1a13812da2f3
aroop <aroop@netopss-MBP-5.Dlink>
2019-11-05 22:18:54 +0530

    log added

--- node/controllers/reports.js
+++ node/controllers/reports.js
@@ -161,6 +161,7 @@ exports.fetchBenchmarkReports = function (req, res) {
                                     callback('error')
                                 }
                                 records_db.map((record) => {
+                                    LOG.info(record._doc._id + record._doc.model_id + '')
                                     if (!parent_ids.includes(record._doc._id + record._doc.model_id + '')) {
                                         word_count += record._doc.source.split(' ').length
                                         record_unique.push(record)
