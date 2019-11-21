c5e6b5cc4bd8e31927e96a981686ede9853425b6
aroop <aroop@netopss-MacBook-Pro-5.local>
2019-11-21 20:44:07 +0530

    threads decreased

--- python/utils/document_writer.py
+++ python/utils/document_writer.py
@@ -13,6 +13,32 @@ TOPIC_TO_PROCESS = 'to-process'
 STATUS_PROCESSED = 'COMPLETED'
 app.config['UPLOAD_FOLDER'] = 'upload'
 
+def write_document_basename(basename):
+    log.info('write_document : started for ' + basename)
+    with app.app_context():
+        filepath = os.path.join(
+                    app.config['UPLOAD_FOLDER'], basename + '_s.docx')
+        filepath_processed = os.path.join(
+                    app.config['UPLOAD_FOLDER'], basename + '_t' + '.docx')
+
+        xml_content = docx_helper.get_document_xml(filepath)
+        xmltree = docx_helper.get_xml_tree(xml_content)
+        nodes = []
+
+        for node, text in docx_helper.itertext_old(xmltree):
+            nodes.append(node)
+        for node in nodes:
+            node_id = node.attrib['id']
+            if node.text is not None and node.text.strip() is not '':
+                text_node = TextNode.objects(node_id=node_id, basename=basename)
+                if text_node is not None:
+                    tgt_text = get_tgt_text(text_node)
+                    node.text = tgt_text
+        docx_helper.save_docx(filepath, xmltree, filepath_processed, None)
+        translationProcess = TranslationProcess.objects(basename=basename)
+        translationProcess.update(set__status=STATUS_PROCESSED)
+        log.info('write_document : ended for ' + basename)
+
 
 def write_document():
     consumer = get_consumer(TOPIC_TO_PROCESS)
