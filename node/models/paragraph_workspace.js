954516fd7d55cbb01657b4d4d43dfc64fa92bb91
aroop <aroop.ghosh@tarento.com>
2019-12-16 15:02:02 +0530

    fetch workspace api aded

--- node/models/paragraph_workspace.js
+++ node/models/paragraph_workspace.js
@@ -10,14 +10,14 @@ var Schema = mongoose.Schema;
 var LOG = require('../logger/logger').logger
 
 var ParagraphWorkspaceSchema = new Schema({
-    title: {type: String},
-    config_file_location: {type: String},
-    csv_file_location: {type: String},
+    title: { type: String },
+    config_file_location: { type: String },
+    csv_file_location: { type: String },
 
 }, { strict: false });
 var ParagraphWorkspace = mongoose.model('paragraph_workspace', ParagraphWorkspaceSchema);
 
-ParagraphWorkspace.save = function(paragraphWorkspaces, cb){
+ParagraphWorkspace.save = function (paragraphWorkspaces, cb) {
     ParagraphWorkspace.collection.insertMany(paragraphWorkspaces, function (err, docs) {
         if (err) {
             return cb(err, null)
@@ -25,6 +25,17 @@ ParagraphWorkspace.save = function(paragraphWorkspaces, cb){
             LOG.info('%s paragraphWorkspaces was successfully stored.', JSON.stringify(docs));
             return cb(null, docs)
         }
+    })
+}
+
+ParagraphWorkspace.findByCondition = function (condition, pagesize, pageno, cb) {
+    ParagraphWorkspace.find(condition, {}, (pagesize && pageno ? { skip: (pageno - 1) * pagesize, limit: parseInt(pagesize) } : {}), function (err, data) {
+        if (err) {
+            LOG.error("Unable to find ParagraphWorkspace due to [%s]", JSON.stringify(err));
+            return cb(err, null);
+        }
+        LOG.info("[%s] ParagraphWorkspace found", data);
+        return cb(null, data);
     })
 }
 
