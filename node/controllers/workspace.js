2b224c58fe3d247bdd5cf352fe41deead9367421
aroop <aroop@netopss-MacBook-Pro-5.local>
2019-12-16 15:55:50 +0530

    code modified for workspace saving

--- node/controllers/workspace.js
+++ node/controllers/workspace.js
@@ -12,6 +12,15 @@ const STATUS_PROCESSING = 'PROCESSING'
 const STATUS_PROCESSED = 'PROCESSED'
 const STEP_UPLOAD_PRAGRAPH = 'UPLOAD_PRAGRAPH'
 
+exports.handleTokenizeRequest = function (req) {
+    if (!req || !req.data || !req.data.processId) {
+        LOG.error('Data missing for [%s]', JSON.stringify(req))
+    } else {
+        ParagraphWorkspace.findOne({ basename: basename }, function (error, corpus) {
+
+        })
+    }
+}
 
 exports.fetchParagraphWorkspace = function (req, res) {
     let status = req.query.status
@@ -42,64 +51,61 @@ exports.fetchParagraphWorkspace = function (req, res) {
 }
 
 exports.saveParagraphWorkspace = function (req, res) {
-    if (!req || !req.body || !req.body.paragraph_workspaces || !Array.isArray(req.body.paragraph_workspaces) || req.body.paragraph_workspaces.length <= 0) {
+    if (!req || !req.body || !req.body.paragraph_workspace) {
         let apistatus = new APIStatus(StatusCode.ERR_GLOBAL_MISSING_PARAMETERS, COMPONENT).getRspStatus()
         return res.status(apistatus.http.status).json(apistatus);
     }
-    req.body.paragraph_workspaces.map((workspace) => {
-        workspace.session_id = UUIDV4()
+    let workspace = req.body.paragraph_workspace
+    workspace.session_id = UUIDV4()
+    fs.mkdir(BASE_PATH_PIPELINE_1 + workspace.session_id, function (e) {
         fs.mkdir(BASE_PATH_PIPELINE_1 + workspace.session_id, function (e) {
-            fs.mkdir(BASE_PATH_PIPELINE_1 + workspace.session_id, function (e) {
-                fs.copyFile('nginx/' + workspace.config_file_location, BASE_PATH_PIPELINE_1 + workspace.session_id + '/' + workspace.config_file_location, function (err) {
-                    if (err) {
-                        LOG.error(err)
-                        let apistatus = new APIStatus(StatusCode.ERR_GLOBAL_SYSTEM, COMPONENT).getRspStatus()
-                        return res.status(apistatus.http.status).json(apistatus);
-                    }
-                    else {
-                        fs.copyFile('nginx/' + workspace.csv_file_location, BASE_PATH_PIPELINE_1 + workspace.session_id + '/' + workspace.csv_file_location, function (err) {
+            fs.copyFile('nginx/' + workspace.config_file_location, BASE_PATH_PIPELINE_1 + workspace.session_id + '/' + workspace.config_file_location, function (err) {
+                if (err) {
+                    LOG.error(err)
+                    let apistatus = new APIStatus(StatusCode.ERR_GLOBAL_SYSTEM, COMPONENT).getRspStatus()
+                    return res.status(apistatus.http.status).json(apistatus);
+                }
+                else {
+                    fs.copyFile('nginx/' + workspace.csv_file_location, BASE_PATH_PIPELINE_1 + workspace.session_id + '/' + workspace.csv_file_location, function (err) {
+                        if (err) {
+                            LOG.error(err)
+                            let apistatus = new APIStatus(StatusCode.ERR_GLOBAL_SYSTEM, COMPONENT).getRspStatus()
+                            return res.status(apistatus.http.status).json(apistatus);
+                        }
+                        workspace.status = STATUS_PROCESSING
+                        workspace.stage = 1
+                        workspace.step = STEP_UPLOAD_PRAGRAPH
+                        ParagraphWorkspace.save([workspace], function (err, models) {
                             if (err) {
-                                LOG.error(err)
                                 let apistatus = new APIStatus(StatusCode.ERR_GLOBAL_SYSTEM, COMPONENT).getRspStatus()
                                 return res.status(apistatus.http.status).json(apistatus);
                             }
-                            req.body.paragraph_workspaces.map((workspace) => {
-                                workspace.status = STATUS_PROCESSING
-                                workspace.stage = 1
-                                workspace.step = STEP_UPLOAD_PRAGRAPH
-                            })
-                            ParagraphWorkspace.save(req.body.paragraph_workspaces, function (err, models) {
-                                if (err) {
-                                    let apistatus = new APIStatus(StatusCode.ERR_GLOBAL_SYSTEM, COMPONENT).getRspStatus()
-                                    return res.status(apistatus.http.status).json(apistatus);
-                                }
-                                models.ops.map((data) => {
-                                    KafkaProducer.getInstance().getProducer((err, producer) => {
-                                        if (err) {
-                                            LOG.error("Unable to connect to KafkaProducer");
-                                        } else {
-                                            LOG.info("KafkaProducer connected")
-                                            let payloads = [
-                                                {
-                                                    topic: 'tokenext', messages: JSON.stringify({ data: data }), partition: 0
-                                                }
-                                            ]
-                                            producer.send(payloads, function (err, data) {
-                                                LOG.info('Produced')
-                                            });
-                                        }
-                                    })
+                            models.ops.map((data) => {
+                                KafkaProducer.getInstance().getProducer((err, producer) => {
+                                    if (err) {
+                                        LOG.error("Unable to connect to KafkaProducer");
+                                    } else {
+                                        LOG.info("KafkaProducer connected")
+                                        let payloads = [
+                                            {
+                                                topic: 'tokenext', messages: JSON.stringify({ data: data }), partition: 0
+                                            }
+                                        ]
+                                        producer.send(payloads, function (err, data) {
+                                            LOG.info('Produced')
+                                        });
+                                    }
                                 })
-
-                                let response = new Response(StatusCode.SUCCESS, COMPONENT).getRsp()
-                                return res.status(response.http.status).json(response);
                             })
-                        });
-                    }
-                });
-            })
-        });
-    })
+
+                            let response = new Response(StatusCode.SUCCESS, COMPONENT).getRsp()
+                            return res.status(response.http.status).json(response);
+                        })
+                    });
+                }
+            });
+        })
+    });
 
 
 }
