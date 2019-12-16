2db97a7d75d8646091325dbdbbbc521caf91e9e3
aroop <aroop.ghosh@tarento.com>
2019-12-16 11:27:22 +0530

    env added for kafka

--- node/controllers/workspace.js
+++ node/controllers/workspace.js
@@ -3,6 +3,7 @@ var APIStatus = require('../errors/apistatus')
 var ParagraphWorkspace = require('../models/paragraph_workspace');
 var StatusCode = require('../errors/statuscodes').StatusCode
 var LOG = require('../logger/logger').logger
+var KafkaProducer = require('../kafka/producer');
 var fs = require('fs');
 var UUIDV4 = require('uuid/v4')
 var COMPONENT = "workspace";
@@ -16,20 +17,43 @@ exports.saveParagraphWorkspace = function (req, res) {
         workspace.session_id = UUIDV4()
         fs.mkdir('corpusfiles/processing/' + workspace.session_id, function (e) {
             fs.mkdir('corpusfiles/processing/' + workspace.session_id + '/pipeline_stage_1', function (e) {
-                fs.copyFile('/tmp/nginx/' + workspace.config_file_location, 'corpusfiles/processing/' + workspace.session_id + '/pipeline_stage_1/' + workspace.config_file_location, function (err) {
+                fs.copyFile('nginx/' + workspace.config_file_location, 'corpusfiles/processing/' + workspace.session_id + '/pipeline_stage_1/' + workspace.config_file_location, function (err) {
                     if (err) {
                         LOG.error(err)
+                        let apistatus = new APIStatus(StatusCode.ERR_GLOBAL_SYSTEM, COMPONENT).getRspStatus()
+                        return res.status(apistatus.http.status).json(apistatus);
                     }
                     else {
-                        fs.copyFile('/tmp/nginx/' + workspace.csv_file_location, 'corpusfiles/processing/' + workspace.session_id + '/pipeline_stage_1/' + workspace.csv_file_location, function (err) {
+                        fs.copyFile('nginx/' + workspace.csv_file_location, 'corpusfiles/processing/' + workspace.session_id + '/pipeline_stage_1/' + workspace.csv_file_location, function (err) {
                             if (err) {
                                 LOG.error(err)
+                                let apistatus = new APIStatus(StatusCode.ERR_GLOBAL_SYSTEM, COMPONENT).getRspStatus()
+                                return res.status(apistatus.http.status).json(apistatus);
                             }
                             ParagraphWorkspace.save(req.body.paragraph_workspaces, function (err, models) {
                                 if (err) {
                                     let apistatus = new APIStatus(StatusCode.ERR_GLOBAL_SYSTEM, COMPONENT).getRspStatus()
                                     return res.status(apistatus.http.status).json(apistatus);
                                 }
+                                LOG.info(models.ops)
+                                models.ops.map((data) => {
+                                    KafkaProducer.getInstance().getProducer((err, producer) => {
+                                        if (err) {
+                                            LOG.error("Unable to connect to KafkaProducer");
+                                        } else {
+                                            LOG.info("KafkaProducer connected")
+                                            let payloads = [
+                                                {
+                                                    topic: 'tokenext', messages: JSON.stringify({ data: data }), partition: 0
+                                                }
+                                            ]
+                                            producer.send(payloads, function (err, data) {
+                                                LOG.info('Produced')
+                                            });
+                                        }
+                                    })
+                                })
+
                                 let response = new Response(StatusCode.SUCCESS, COMPONENT).getRsp()
                                 return res.status(response.http.status).json(response);
                             })
