5d15029bd4e71aab8e1328e22c724921cae2cd55
aroop <aroop@netopss-MacBook-Pro-5.local>
2019-12-17 17:43:13 +0530

    upload removed

--- node/app.js
+++ node/app.js
@@ -100,7 +100,6 @@ function startApp() {
     extended: false
   }));
   app.use(methodOverride());
-  app.use(upload.any());
   app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
