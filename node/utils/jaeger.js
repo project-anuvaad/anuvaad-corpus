f0ca36a2d9181e6cb48140e9bff5b4e51256f87c
aroop <aroop@netopss-MacBook-Pro-5.local>
2020-01-17 17:10:03 +0530

    reporter modified

--- node/utils/jaeger.js
+++ node/utils/jaeger.js
@@ -3,22 +3,21 @@ var LOG = require('../logger/logger').logger
 
 
 function initTracer(serviceName) {
-    LOG.info(process.env.JAEGER_SERVICE)
     const config = {
         serviceName: serviceName,
-        reporter: {
-            // Provide the traces endpoint; this forces the client to connect directly to the Collector and send
-            // spans over HTTP
-            collectorEndpoint: process.env.JAEGER_SERVICE ? process.env.JAEGER_SERVICE : 'http://localhost:14268/api/traces',
-            // Provide username and password if authentication is enabled in the Collector
-            // username: '',
-            // password: '',
-        },
+        // reporter: {
+        //     // Provide the traces endpoint; this forces the client to connect directly to the Collector and send
+        //     // spans over HTTP
+        //     // Provide username and password if authentication is enabled in the Collector
+        //     // username: '',
+        //     // password: '',
+        // },
         sampler: {
             type: "const",
             param: 1,
         },
         reporter: {
+            collectorEndpoint: process.env.JAEGER_SERVICE ? process.env.JAEGER_SERVICE : 'http://localhost:14268/api/traces',
             logSpans: true,
         },
     };
