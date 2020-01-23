a35b87364eef9de0d703e07f76c68f41ecc4da84
aroop <aroop@netopss-MacBook-Pro-5.local>
2020-01-23 15:27:15 +0530

    jeger collector with root span addded

--- node/utils/jaeger-collector.js
+++ node/utils/jaeger-collector.js
@@ -1,28 +1,29 @@
 var jaeger = require('./jaeger')
-const tracer = jaeger("anuvaad-node");
+const tracer = jaeger("anuvaad");
 
 
 function jaegerCollector(req, res, next) {
-    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    var method = req.method;
-    var url = req.url;
-    var referer = req.headers.referer || "";
-    var ua = req.headers['user-agent'];
-    let span = tracer.startSpan(url)
-    span.setTag("http.method", method);
-    span.setTag("http.referer", referer);
-    span.setTag("http.user-agent", ua);
-    span.setTag("http.ip", ip);
+    if (req.rootSpan) {
+        var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        var method = req.method;
+        var referer = req.headers.referer || "";
+        var ua = req.headers['user-agent'];
+        let span = tracer.startSpan('node-app', { childOf: req.rootSpan })
+        span.setTag("http.method", method);
+        span.setTag("http.referer", referer);
+        span.setTag("http.user-agent", ua);
+        span.setTag("http.ip", ip);
 
 
-    res.on('finish', () => {
-        var code = res._header ? String(res.statusCode) : String(-1);
-        var message = res._header ? String(res.statusMessage) : String(-1);
-        span.setTag("http.status_code", code);
-        span.setTag("http.status_message", message);
-        span.log({ 'event': 'request_end' });
-        span.finish();
-    });
+        res.on('finish', () => {
+            var code = res._header ? String(res.statusCode) : String(-1);
+            var message = res._header ? String(res.statusMessage) : String(-1);
+            span.setTag("http.status_code", code);
+            span.setTag("http.status_message", message);
+            span.log({ 'event': 'request_end' });
+            span.finish();
+        });
+    }
     next()
 }
 
