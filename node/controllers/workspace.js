c2ad98f123378b411555fc8269206f3ca0e34d2f
aroop <aroop@netopss-MacBook-Pro-5.local>
2020-02-14 15:51:50 +0530

    .

--- node/controllers/workspace.js
+++ node/controllers/workspace.js
@@ -1207,7 +1207,7 @@ exports.saveMTWorkspace = function (req, res) {
                                 }
                             ]
                             LOG.debug('Sending message', payloads)
-                            axios.post(CORPUS_REPORT_URL, { session_id: workspace.session_id, files: workspace.selected_files, target_language: workspace.target_lang }).then((api_res) => {
+                            axios.post(CORPUS_REPORT_URL, { session_id: workspace.session_id, files: workspace.selected_files, target_language: workspace.target_lang }, { headers: { 'content-type': 'application/json' } }).then((api_res) => {
                                 LOG.debug('Response receive for mt report')
                                 LOG.debug(api_res.data)
                                 if (api_res && api_res.data) {
