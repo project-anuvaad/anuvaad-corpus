c5b1c9e516458e31acebab449f7a98c6c35e2dca
aroop <aroop@netopss-MacBook-Pro-5.local>
2020-03-05 14:16:53 +0530

    ft added in abbrevations

--- python/utils/anuvaad_tokenizer.py
+++ python/utils/anuvaad_tokenizer.py
@@ -14,7 +14,7 @@ class AnuvaadEngTokenizer(object):
     """
     Default abbrevations
     """
-    _abbrevations_with_space = ['of ', 'NO. ','Pvt. ', 'NOS. ','Smt. ','Sec. ','Spl. ','Mr. ']
+    _abbrevations_with_space = ['of ', 'NO. ','Pvt. ', 'NOS. ','Smt. ','Sec. ','Spl. ','Mr. ','ft. ']
     _abbrevations_without_space = ['Crl.']
     _tokenizer = None
     _regex_search_texts = []
