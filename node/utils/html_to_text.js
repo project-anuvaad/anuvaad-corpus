7398a9f578f308b330e5285edf9efce97d5ed0e0
aroop <aroop.ghosh@tarento.com>
2020-03-12 09:54:49 +0530

    pagewise data added for pdf to text

--- node/utils/html_to_text.js
+++ node/utils/html_to_text.js
@@ -3,6 +3,46 @@ const fs = require('fs');
 const sentence_ends = ['.', '?', '!']
 const regex = /([,|a-zA-Z]{2,}[.]$)/g;
 
+exports.convertHtmlToJsonPagewise = function (basefolder, inputfilename, session_id, cb) {
+    fs.readFile(basefolder + session_id + "/" + inputfilename, 'utf8', function (err, data) {
+        let output = []
+        data = data.replace(/<br\/>/g, ' ')
+        htmlToJson.parse(data, function () {
+            return this.map('p', function ($item) {
+                let obj = {}
+                let style = $item['0']['attribs']['style']
+                let class_identifier = $item['0']['attribs']['class']
+                let styles = style.split(';')
+                styles.map((s) => {
+                    let topLeft = s.split(':')
+                    if (topLeft[0] == 'left') {
+                        obj.x = topLeft[1].split('px')[0]
+                    } else if (topLeft[0] == 'top') {
+                        obj.y = topLeft[1].split('px')[0]
+                    }
+                })
+                obj.text = $item.text()
+                obj.style = style
+                obj['class'] = class_identifier
+                return obj;
+            });
+        }).done(function (items) {
+            items.map((it, index) => {
+                if (output && output.length > 0) {
+                    let data = output[output.length - 1]
+                    data.text += ' ' + it.text
+                    data.text = data.text.replace(/\s+/g, " ")
+                } else {
+                    output.push(it)
+                }
+            })
+            cb(null, output)
+        }, function (err) {
+            cb(err, null)
+        });
+    })
+}
+
 
 exports.convertHtmlToJson = function (basefolder, inputfilename, session_id, cb) {
     fs.readFile(basefolder + session_id + "/" + inputfilename, 'utf8', function (err, data) {
