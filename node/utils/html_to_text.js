c87cc6464223e8f3e185a9e9883071c598f69d43
aroop <aroop@netopss-MBP-5.Dlink>
2020-03-17 15:51:21 +0530

    algo modified for sentence extraction

--- node/utils/html_to_text.js
+++ node/utils/html_to_text.js
@@ -2,6 +2,9 @@ const htmlToJson = require('html-to-json')
 const fs = require('fs');
 const sentence_ends = ['.', '?', '!']
 const regex = /([,|a-zA-Z]{2,}[.]$)/g;
+const abbrivations2 = ['no.', 'mr.', 'ft.', 'kg.', 'dr.', 'ms.', 'st.', 'pp.', 'co.', 'rs.', 'sh.', 'vs.']
+const abbrivations3 = ['pvt.', 'nos.', 'smt.', 'sec.', 'spl.', 'kgs.', 'ltd.', 'pty.', 'vol.', 'pty.', 'm/s.', 'mrs.']
+const abbrivations4 = ['assn.']
 
 exports.convertHtmlToJsonPagewise = function (basefolder, inputfilename, session_id, cb) {
     fs.readFile(basefolder + session_id + "/" + inputfilename, 'utf8', function (err, data) {
@@ -32,6 +35,7 @@ exports.convertHtmlToJsonPagewise = function (basefolder, inputfilename, session
                     let data = output[output.length - 1]
                     data.text += ' ' + it.text
                     data.text = data.text.replace(/\s+/g, " ")
+                    data.text = data.text.replace(/Digitally signed by.{1,}Signature Not Verified/gm, '')
                 } else {
                     output.push(it)
                 }
@@ -47,8 +51,14 @@ exports.convertHtmlToJsonPagewise = function (basefolder, inputfilename, session
 exports.convertHtmlToJson = function (basefolder, inputfilename, session_id, cb) {
     fs.readFile(basefolder + session_id + "/" + inputfilename, 'utf8', function (err, data) {
         let output = []
+        let style_map = {}
         data = data.replace(/<br\/>/g, ' ')
         htmlToJson.parse(data, function () {
+            var style_text = ''
+            this.map('style', function ($item) {
+                style_text += $item.toString()
+            })
+            var node_index = 0
             return this.map('p', function ($item) {
                 let obj = {}
                 let style = $item['0']['attribs']['style']
@@ -65,24 +75,63 @@ exports.convertHtmlToJson = function (basefolder, inputfilename, session_id, cb)
                 obj.text = $item.text()
                 obj.style = style
                 obj['class'] = class_identifier
+                obj['page_no'] = parseInt(class_identifier.split('ft')[1] / 10)
+                let class_style_text = style_text.split(obj['class'])[1].split('}')[0].split('{')[1]
+                let class_style_obj = {}
+                class_style_text = class_style_text.split(';')
+                class_style_text.map((c) => {
+                    class_style_obj[c.split(':')[0]] = c.split(':')[1]
+                })
+                obj['class_style'] = class_style_obj
+                obj.node_index = node_index
+                node_index++
                 return obj;
             });
         }).done(function (items) {
             items.map((it, index) => {
+                let class_identifier = it.class_style['font-size'] + it.class_style['font-family']
                 if (output && output.length > 0) {
                     let data = output[output.length - 1]
-                    if ((data.y == it.y || !(sentence_ends.indexOf(data.text.substring(data.text.length - 1, data.text.length)) >= 0 && data.text.search(regex) >= 0)) && index > 10) {
-                        data.text += ' ' + it.text
-                        data.text = data.text.replace(/\s+/g, " ")
+                    // if ((data.y == it.y || !(sentence_ends.indexOf(data.text.substring(data.text.length - 1, data.text.length)) >= 0 && data.text.search(regex) >= 0)) && data.class_style === it.class_style) {
+                    //     data.text += ' ' + it.text
+                    //     data.text = data.text.replace(/\s+/g, " ")
+                    // } else {
+                    if (style_map[class_identifier] && it.page_no - style_map[class_identifier].data.page_no <= 1) {
+                        let old_data = style_map[class_identifier]
+                        let data = old_data.data
+                        if ((!(sentence_ends.indexOf(data.text.substring(data.text.length - 1, data.text.length)) >= 0 && data.text.search(regex) >= 0) || abbrivations2.indexOf(data.text.substring(data.text.length - 3, data.text.length).toLowerCase()) >= 0 || abbrivations3.indexOf(data.text.substring(data.text.length - 4, data.text.length).toLowerCase()) >= 0 || abbrivations4.indexOf(data.text.substring(data.text.length - 5, data.text.length).toLowerCase()) >= 0) && it.node_index - data.node_index <= 5) {
+                            if (it.node_index - data.node_index >= 2 && it.page_no - style_map[class_identifier].data.page_no == 0) {
+                                output.push(it)
+                                style_map[class_identifier] = { index: output.length - 1, data: it }
+                            } else {
+                                old_data.data.text += " " + it.text.replace(/\s+/g, " ")
+                                old_data.data.node_index = it.node_index
+                                output[old_data.index] = old_data.data
+                                style_map[class_identifier] = old_data
+                            }
+                        } else {
+                            output.push(it)
+                            style_map[class_identifier] = { index: output.length - 1, data: it }
+                        }
                     } else {
-                        it.text = it.text.replace(/\s+/g, " ")
                         output.push(it)
+                        style_map[class_identifier] = { index: output.length - 1, data: it }
                     }
+                    // }
                 } else {
                     output.push(it)
+                    style_map[class_identifier] = { index: output.length - 1, data: it }
                 }
             })
-            cb(null, output)
+            var out = output.filter((o) => {
+                o.text = o.text.replace(/\s+/g, " ")
+                o.text = o.text.replace(/Digitally signed by.{1,}Signature Not Verified/gm, '')
+                if (o.text != o.page_no && o.text.length > 0) {
+                    return true
+                }
+                return false
+            })
+            cb(null, out)
         }, function (err) {
             cb(err, null)
         });
