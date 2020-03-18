2df7e9668d54c1d05369d2c1909718468300d37a
aroop <aroop@netopss-MBP-5.Dlink>
2020-03-18 13:17:44 +0530

    header footer removal added

--- node/utils/html_to_text.js
+++ node/utils/html_to_text.js
@@ -58,8 +58,10 @@ exports.convertHtmlToJson = function (basefolder, inputfilename, session_id, cb)
             this.map('style', function ($item) {
                 style_text += $item.toString()
             })
-            var node_index = 0
-            return this.map('p', function ($item) {
+            var node_index = 1
+            var previous_page = 0
+            var pagewise_data = {}
+            this.map('p', function ($item) {
                 let obj = {}
                 let style = $item['0']['attribs']['style']
                 let class_identifier = $item['0']['attribs']['class']
@@ -77,6 +79,10 @@ exports.convertHtmlToJson = function (basefolder, inputfilename, session_id, cb)
                 obj['class'] = class_identifier
                 obj['page_no'] = parseInt(class_identifier.split('ft')[1] / 10)
                 obj['page_no_end'] = parseInt(class_identifier.split('ft')[1] / 10)
+                if (previous_page !== obj['page_no_end']) {
+                    previous_page = obj['page_no_end']
+                    node_index = 1
+                }
                 let class_style_text = style_text.split(obj['class'])[1].split('}')[0].split('{')[1]
                 let class_style_obj = {}
                 class_style_text = class_style_text.split(';')
@@ -86,45 +92,99 @@ exports.convertHtmlToJson = function (basefolder, inputfilename, session_id, cb)
                 obj['class_style'] = class_style_obj
                 obj.node_index = node_index
                 node_index++
-                return obj;
+                if (pagewise_data[obj['page_no']]) {
+                    let data = pagewise_data[obj['page_no']]
+                    data.push(obj)
+                } else {
+                    let data = [obj]
+                    pagewise_data[obj['page_no']] = data
+                }
+
             });
+            return pagewise_data;
         }).done(function (items) {
-            items.map((it, index) => {
-                let class_identifier = it.class_style['font-size'] + it.class_style['font-family']
-                if (output && output.length > 0) {
-                    let data = output[output.length - 1]
-                    // if ((data.y == it.y || !(sentence_ends.indexOf(data.text.substring(data.text.length - 1, data.text.length)) >= 0 && data.text.search(regex) >= 0)) && data.class_style === it.class_style) {
-                    //     data.text += ' ' + it.text
-                    //     data.text = data.text.replace(/\s+/g, " ")
-                    // } else {
-                    if (style_map[class_identifier] && it.page_no_end - style_map[class_identifier].data.page_no_end <= 1) {
-                        let old_data = style_map[class_identifier]
-                        let data = old_data.data
-                        data.text = data.text.trim()
-                        if ((!(sentence_ends.indexOf(data.text.substring(data.text.length - 1, data.text.length)) >= 0 && data.text.search(regex) >= 0) || abbrivations2.indexOf(data.text.substring(data.text.length - 3, data.text.length).toLowerCase()) >= 0 || abbrivations3.indexOf(data.text.substring(data.text.length - 4, data.text.length).toLowerCase()) >= 0 || abbrivations4.indexOf(data.text.substring(data.text.length - 5, data.text.length).toLowerCase()) >= 0) && it.node_index - data.node_index <= 5) {
-                            if (it.node_index - data.node_index >= 2 && it.page_no - style_map[class_identifier].data.page_no == 0) {
+            //Find header or footer
+            let header_end_index = -1
+            let footer_start_index = -1
+            Object.keys(items).forEach(function (key, index) {
+                let obj = items[key]
+                let obj_prev = items[key - 1]
+                let obj_next = items[key + 1]
+                let obj_to_check = obj_prev ? obj_prev : obj_next
+                let current_header_end_index = -1
+                let current_footer_start_index = -1
+                obj.map((it, index) => {
+                    if (current_header_end_index == -1 || index - current_header_end_index == 1) {
+                        if (obj_to_check[index] && obj_to_check[index].text === obj[index].text) {
+                            current_header_end_index = index
+                        }
+                    }
+                })
+                obj.slice(0).reverse().map((it, index) => {
+                    if (current_footer_start_index == -1 || index - current_footer_start_index == 1) {
+                        console.log(obj_to_check.length - index-1)
+                        console.log(obj_to_check[obj_to_check.length - index-1])
+                        if (obj_to_check[obj_to_check.length - index-1] && obj_to_check[obj_to_check.length - index-1].text === obj[index].text) {
+                            current_footer_start_index = index
+                        }
+                    }
+                })
+                if (header_end_index !== -1 && header_end_index !== current_header_end_index) {
+                    header_end_index = -1
+                } else {
+                    header_end_index = current_header_end_index
+                }
+                if (footer_start_index !== -1 && footer_start_index !== current_footer_start_index) {
+                    footer_start_index = -1
+                } else {
+                    footer_start_index = current_footer_start_index
+                }
+            })
+            Object.keys(items).forEach(function (key, index) {
+                let obj = items[key]
+                obj.map((it, index) => {
+                    if (it.text == it.page_no) {
+                        return
+                    }
+                    if (header_end_index !== -1 && index <= header_end_index) {
+                        return
+                    }
+                    let class_identifier = it.class_style['font-size'] + it.class_style['font-family']
+                    if (output && output.length > 0) {
+                        let data = output[output.length - 1]
+                        // if ((data.y == it.y || !(sentence_ends.indexOf(data.text.substring(data.text.length - 1, data.text.length)) >= 0 && data.text.search(regex) >= 0)) && data.class_style === it.class_style) {
+                        //     data.text += ' ' + it.text
+                        //     data.text = data.text.replace(/\s+/g, " ")
+                        // } else {
+                        if (style_map[class_identifier] && it.page_no_end - style_map[class_identifier].data.page_no_end <= 1) {
+                            let old_data = style_map[class_identifier]
+                            let data = old_data.data
+                            data.text = data.text.trim()
+                            if ((!(sentence_ends.indexOf(data.text.substring(data.text.length - 1, data.text.length)) >= 0 && data.text.search(regex) >= 0) || abbrivations2.indexOf(data.text.substring(data.text.length - 3, data.text.length).toLowerCase()) >= 0 || abbrivations3.indexOf(data.text.substring(data.text.length - 4, data.text.length).toLowerCase()) >= 0 || abbrivations4.indexOf(data.text.substring(data.text.length - 5, data.text.length).toLowerCase()) >= 0) && it.node_index - data.node_index <= 5) {
+                                if (it.node_index - data.node_index >= 2 && it.page_no - style_map[class_identifier].data.page_no == 0) {
+                                    output.push(it)
+                                    style_map[class_identifier] = { index: output.length - 1, data: it }
+                                } else {
+                                    old_data.data.text += " " + it.text.replace(/\s+/g, " ")
+                                    old_data.data.node_index = it.node_index
+                                    old_data.data.page_no_end = it.page_no_end
+                                    output[old_data.index] = old_data.data
+                                    style_map[class_identifier] = old_data
+                                }
+                            } else {
                                 output.push(it)
                                 style_map[class_identifier] = { index: output.length - 1, data: it }
-                            } else {
-                                old_data.data.text += " " + it.text.replace(/\s+/g, " ")
-                                old_data.data.node_index = it.node_index
-                                old_data.data.page_no_end = it.page_no_end
-                                output[old_data.index] = old_data.data
-                                style_map[class_identifier] = old_data
                             }
                         } else {
                             output.push(it)
                             style_map[class_identifier] = { index: output.length - 1, data: it }
                         }
+                        // }
                     } else {
                         output.push(it)
                         style_map[class_identifier] = { index: output.length - 1, data: it }
                     }
-                    // }
-                } else {
-                    output.push(it)
-                    style_map[class_identifier] = { index: output.length - 1, data: it }
-                }
+                })
             })
             var out = output.filter((o) => {
                 o.text = o.text.replace(/\s+/g, " ")
