5d6dbd42bfc22bd3e2e10bb408614d6ab008331d
aroop <aroop@netopss-MBP-5.Dlink>
2020-03-19 12:31:33 +0530

    footer fixed

--- node/utils/html_to_text.js
+++ node/utils/html_to_text.js
@@ -6,11 +6,16 @@ const abbrivations2 = ['no.', 'mr.', 'ft.', 'kg.', 'dr.', 'ms.', 'st.', 'pp.', '
 const abbrivations3 = ['pvt.', 'nos.', 'smt.', 'sec.', 'spl.', 'kgs.', 'ltd.', 'pty.', 'vol.', 'pty.', 'm/s.', 'mrs.']
 const abbrivations4 = ['assn.']
 
-exports.convertHtmlToJsonPagewise = function (basefolder, inputfilename, session_id, cb) {
+exports.convertHtmlToJsonPagewise = function (basefolder, inputfilename, session_id, merge, pageno, cb) {
     fs.readFile(basefolder + session_id + "/" + inputfilename, 'utf8', function (err, data) {
         let output = []
         data = data.replace(/<br\/>/g, ' ')
         htmlToJson.parse(data, function () {
+            var style_text = ''
+            this.map('style', function ($item) {
+                style_text += $item.toString()
+            })
+            var node_index = 1
             return this.map('p', function ($item) {
                 let obj = {}
                 let style = $item['0']['attribs']['style']
@@ -27,20 +32,35 @@ exports.convertHtmlToJsonPagewise = function (basefolder, inputfilename, session
                 obj.text = $item.text()
                 obj.style = style
                 obj['class'] = class_identifier
+                obj['page_no'] = pageno
+                obj['page_no_end'] = pageno
+                let class_style_text = style_text.split(obj['class'])[1].split('}')[0].split('{')[1]
+                let class_style_obj = {}
+                class_style_text = class_style_text.split(';')
+                class_style_text.map((c) => {
+                    class_style_obj[c.split(':')[0]] = c.split(':')[1]
+                })
+                obj['class_style'] = class_style_obj
+                obj.node_index = node_index
+                node_index++
                 return obj;
             });
         }).done(function (items) {
-            items.map((it, index) => {
-                if (output && output.length > 0) {
-                    let data = output[output.length - 1]
-                    data.text += ' ' + it.text
-                    data.text = data.text.replace(/\s+/g, " ")
-                    data.text = data.text.replace(/Digitally signed by.{1,}Signature Not Verified/gm, '')
-                } else {
-                    output.push(it)
-                }
-            })
-            cb(null, output)
+            if (merge) {
+                items.map((it, index) => {
+                    if (output && output.length > 0) {
+                        let data = output[output.length - 1]
+                        data.text += ' ' + it.text
+                        data.text = data.text.replace(/\s+/g, " ")
+                        data.text = data.text.replace(/Digitally signed by.{1,}Signature Not Verified/gm, '')
+                    } else {
+                        output.push(it)
+                    }
+                })
+                cb(null, output)
+            } else {
+                cb(null, items)
+            }
         }, function (err) {
             cb(err, null)
         });
@@ -207,4 +227,110 @@ exports.convertHtmlToJson = function (basefolder, inputfilename, session_id, cb)
             cb(err, null)
         });
     })
+}
+
+
+exports.mergeHtmlNodes = function (items, cb) {
+    let output = []
+    let style_map = {}
+    //Find header or footer
+    let header_end_index = -1
+    let footer_start_index = -1
+    let footer_text = ''
+    Object.keys(items).forEach(function (key, index) {
+        if (index != 0) {
+            let obj = items[key]
+            let obj_prev = items[key - 1]
+            let obj_next = items[key + 1]
+            let obj_to_check = obj_next ? obj_next : obj_prev
+            let current_header_end_index = -1
+            let current_footer_start_index = -1
+            if (obj_to_check) {
+                obj.map((it, index) => {
+                    if (current_header_end_index == -1 || index - current_header_end_index == 1) {
+                        if (obj_to_check[index] && obj_to_check[index].text === obj[index].text) {
+                            current_header_end_index = index
+                        }
+                    }
+                })
+                obj.slice(0).reverse().map((it, index) => {
+                    if (current_footer_start_index == -1 || index - current_footer_start_index == 1) {
+                        if (obj_to_check[obj_to_check.length - index - 1] && obj_to_check[obj_to_check.length - index - 1].text === it.text) {
+                            current_footer_start_index = index
+                            footer_text = it.text
+                        }
+                    } 
+                })
+            }
+            if (header_end_index !== -1 && header_end_index !== current_header_end_index) {
+                header_end_index = -1
+            } else {
+                header_end_index = current_header_end_index
+            }
+            if (footer_start_index !== -1 && footer_start_index !== current_footer_start_index) {
+                footer_start_index = -1
+                footer_text = ''
+            } else {
+                footer_start_index = current_footer_start_index
+            }
+        }
+    })
+    Object.keys(items).forEach(function (key, index) {
+        let obj = items[key]
+        obj.map((it, index) => {
+            if (it.text == it.page_no) {
+                return
+            }
+            if (header_end_index !== -1 && index <= header_end_index) {
+                return
+            }
+            if ((footer_start_index !== -1 && index >= obj.length - footer_start_index - 1) || key == 1 && it.text === footer_text) {
+                return
+            }
+            let class_identifier = it.class_style['font-size'] + it.class_style['font-family']
+            if (output && output.length > 0) {
+                let data = output[output.length - 1]
+                // if ((data.y == it.y || !(sentence_ends.indexOf(data.text.substring(data.text.length - 1, data.text.length)) >= 0 && data.text.search(regex) >= 0)) && data.class_style === it.class_style) {
+                //     data.text += ' ' + it.text
+                //     data.text = data.text.replace(/\s+/g, " ")
+                // } else {
+                if (style_map[class_identifier] && it.page_no_end - style_map[class_identifier].data.page_no_end <= 1) {
+                    let old_data = style_map[class_identifier]
+                    let data = old_data.data
+                    data.text = data.text.trim()
+                    if ((!(sentence_ends.indexOf(data.text.substring(data.text.length - 1, data.text.length)) >= 0 && data.text.search(regex) >= 0) || abbrivations2.indexOf(data.text.substring(data.text.length - 3, data.text.length).toLowerCase()) >= 0 || abbrivations3.indexOf(data.text.substring(data.text.length - 4, data.text.length).toLowerCase()) >= 0 || abbrivations4.indexOf(data.text.substring(data.text.length - 5, data.text.length).toLowerCase()) >= 0) && it.node_index - data.node_index <= 5) {
+                        if (it.node_index - data.node_index > 2 && it.page_no - style_map[class_identifier].data.page_no == 0) {
+                            output.push(it)
+                            style_map[class_identifier] = { index: output.length - 1, data: it }
+                        } else {
+                            old_data.data.text += " " + it.text.replace(/\s+/g, " ")
+                            old_data.data.node_index = it.node_index
+                            old_data.data.page_no_end = it.page_no_end
+                            output[old_data.index] = old_data.data
+                            style_map[class_identifier] = old_data
+                        }
+                    } else {
+                        output.push(it)
+                        style_map[class_identifier] = { index: output.length - 1, data: it }
+                    }
+                } else {
+                    output.push(it)
+                    style_map[class_identifier] = { index: output.length - 1, data: it }
+                }
+                // }
+            } else {
+                output.push(it)
+                style_map[class_identifier] = { index: output.length - 1, data: it }
+            }
+        })
+    })
+    var out = output.filter((o) => {
+        o.text = o.text.replace(/\s+/g, " ")
+        o.text = o.text.replace(/Digitally signed by.{1,}Signature Not Verified/gm, '')
+        if (o.text != o.page_no && o.text.length > 0) {
+            return true
+        }
+        return false
+    })
+    cb(null, out)
 }
