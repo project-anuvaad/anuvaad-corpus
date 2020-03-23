d57129b4f195e107dae9c6b09dda5ed4eb42428b
aroop <aroop@netopss-MBP-5.Dlink>
2020-03-23 16:25:59 +0530

    .

--- node/utils/html_to_text.js
+++ node/utils/html_to_text.js
@@ -1,7 +1,7 @@
 const htmlToJson = require('html-to-json')
 const fs = require('fs');
 var LOG = require('../logger/logger').logger
-const sentence_ends_regex = /([,|a-zA-Z|0-9]{3,}[.|?|!|\"|”|:]$)/g;
+const sentence_ends_regex = /(([,|a-zA-Z|0-9]{3,}[.|?|!|\"|”|:]|([:][ ][-]))$)/g;
 const abbrivations2 = ['no.', 'mr.', 'ft.', 'kg.', 'dr.', 'ms.', 'st.', 'pp.', 'co.', 'rs.', 'sh.', 'vs.']
 const abbrivations3 = ['pvt.', 'nos.', 'smt.', 'sec.', 'spl.', 'kgs.', 'ltd.', 'pty.', 'vol.', 'pty.', 'm/s.', 'mrs.']
 const abbrivations4 = ['assn.']
