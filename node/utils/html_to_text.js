b530c44a9c571a613ca9689c7ce23b41bb699057
aroop <aroop@netopss-MBP-6.Dlink>
2020-04-15 13:27:32 +0530

    underline logic modified

--- node/utils/html_to_text.js
+++ node/utils/html_to_text.js
@@ -251,7 +251,7 @@ exports.mergeHtmlNodes = function (items, cb) {
                     if (line.y !== footer_coordinate) {
                         let y_margin = (parseInt(line.y) - (parseInt(it.y) + parseInt(it.class_style['font-size'].split('px')[0]))) / bottom_px
                         let x_margin = (parseInt(line.x) - parseInt(it.x)) / parseInt(line.x)
-                        if (((y_margin >= 0 && y_margin * 100 < 7) || (parseInt(line.y) >= parseInt(it.y) && parseInt(line.y) <= (parseInt(it.y) + parseInt(it.class_style['font-size'].split('px')[0])))) && Math.abs(x_margin) * 100 < 5) {
+                        if (((y_margin >= 0 && y_margin * 100 < 4) || (parseInt(line.y) >= parseInt(it.y) && parseInt(line.y) <= (parseInt(it.y) + parseInt(it.class_style['font-size'].split('px')[0])))) && Math.abs(x_margin) * 100 < 3) {
                             it.underline = true
                         }
                     }
