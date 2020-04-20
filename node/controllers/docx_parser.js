e2243373b98397d1a23f526bc66215dfed280acb
aroop <aroop@netopss-MBP-6.Dlink>
2020-04-20 19:09:41 +0530

    filepath added

--- node/controllers/docx_parser.js
+++ node/controllers/docx_parser.js
@@ -183,7 +183,7 @@ function processHtml(pdf_parser_process, index, output_res, merge, start_node_in
                                         let apistatus = new APIStatus(StatusCode.ERR_GLOBAL_SYSTEM, COMPONENT).getRspStatus()
                                         return res.status(apistatus.http.status).json(apistatus);
                                     }
-                                    let response = new Response(StatusCode.SUCCESS, COMPONENT).getRsp()
+                                    let response = new Response(StatusCode.SUCCESS, filepath).getRsp()
                                     return res.status(response.http.status).json(response);
                                 })
                             } else {
