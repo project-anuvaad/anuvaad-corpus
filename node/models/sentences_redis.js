076fac02bfa7e0f70710c0afae9f516fbcc71d6d
aroop <aroop@netopss-MBP-6.Dlink>
2020-05-04 13:15:22 +0530

    redis save added

---
+++ node/models/sentences_redis.js
@@ -0,0 +1,26 @@
+var redis_client = require("../db/redis");
+var crypto = require('crypto');
+var LOG = require('../logger/logger').logger
+
+
+exports.saveSentence = function (sentence, userid, cb) {
+    redis_client.set(userid + '_' + crypto.createHash('sha256').update(sentence.source).digest('hex'), sentence, function (err, doc) {
+        if (err) {
+            LOG.error(err)
+            cb(err, null)
+        } else {
+            cb(null, doc)
+        }
+    });
+}
+
+exports.fetchSentence = function (sentence, userid, cb) {
+    redis_client.get(userid + '_' + crypto.createHash('sha256').update(sentence.source).digest('hex'), function (err, doc) {
+        if (err) {
+            LOG.error(err)
+            cb(err, null)
+        } else {
+            cb(null, doc)
+        }
+    });
+}
