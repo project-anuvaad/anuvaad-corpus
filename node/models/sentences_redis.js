c3c4df454c830409bb9cb22c944129be15736a8c
aroop <aroop@netopss-MBP-6.Dlink>
2020-05-07 15:42:26 +0530

    .

--- node/models/sentences_redis.js
+++ node/models/sentences_redis.js
@@ -4,14 +4,18 @@ var LOG = require('../logger/logger').logger
 
 
 exports.saveSentence = function (sentence, userid, cb) {
-    redis_client.set(userid + '_' + crypto.createHash('sha256').update(sentence.source).digest('hex'), JSON.stringify(sentence), function (err, doc) {
-        if (err) {
-            LOG.error(err)
-            cb(err, null)
-        } else {
-            cb(null, doc)
-        }
-    });
+    if (sentence && sentence.source) {
+        redis_client.set(userid + '_' + crypto.createHash('sha256').update(sentence.source).digest('hex'), JSON.stringify(sentence), function (err, doc) {
+            if (err) {
+                LOG.error(err)
+                cb(err, null)
+            } else {
+                cb(null, doc)
+            }
+        });
+    }else{
+        cb(null, {})
+    }
 }
 
 exports.fetchSentence = function (sentence, userid, cb) {
