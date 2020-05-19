7a33757377df8f52042a80fbf9ab0b44d86e4684
aroop <aroop.ghosh@tarento.com>
2020-05-19 15:43:00 +0530

    anuvadd translation added

---
+++ node/utils/translate_anuvaad.js
@@ -0,0 +1,27 @@
+var axios = require('axios');
+var LOG = require('../logger/logger').logger
+const NMT_BASE_URL = process.env.NMT_BASE_URL ? process.env.NMT_BASE_URL : 'http://auth.anuvaad.org'
+var UUIDV4 = require('uuid/v4')
+
+
+exports.translateFromAnuvaad = function (sentences, url_end_point, cb) {
+    axios.post(NMT_BASE_URL + '/' + url_end_point ? url_end_point : 'translate-anuvaad', {
+        sentences
+    }
+        , {
+            headers: {
+                'Content-Type': `application/json`,
+            },
+            timeout: 30000,
+        }
+    ).then(function (api_res) {
+        if (api_res && api_res.data && api_res.data.response_body) {
+            cb(null, api_res.data.response_body)
+        } else {
+            cb('error', null)
+        }
+    }).catch(e => {
+        LOG.error(e)
+        cb(e, null)
+    })
+}
