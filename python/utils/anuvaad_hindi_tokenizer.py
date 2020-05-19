e9069f98a3dd9892bd44829dc8a8949103ab74cf
aroop <aroop@netopss-MBP-6.Dlink>
2020-05-19 14:44:44 +0530

    tokenizer modified for dots

--- python/utils/anuvaad_hindi_tokenizer.py
+++ python/utils/anuvaad_hindi_tokenizer.py
@@ -104,7 +104,7 @@ class AnuvaadHinTokenizer(object):
         return text
 
     def serialize_table_points(self, text):
-        patterns = re.findall(r'(?:(?:(?:[ ][(]?(?:(?:[0,9]|[i]|[x]|[v]){1,3}|[a-zA-Z\u0900-\u097F]{1,1})[)])|(?:[ ](?:(?:[0-9]|[i]|[x]|[v]){1,3}|[a-zA-Z\u0900-\u097F]{1,1})[.])))',text)
+        patterns = re.findall(r'(?:(?:(?:[ ][(]?(?:(?:[0,9]|[i]|[x]|[v]){1,3}|[a-zA-Z\u0900-\u097F]{1,1})[)])|(?:[ ](?:(?:[0-9]|[i]|[x]|[v]){1,3}|[a-zA-Z\u0900-\u097F]{1,1})[.][ ])))',text)
         index = 0
         if patterns is not None and isinstance(patterns, list):
             for pattern in patterns:
@@ -205,7 +205,7 @@ class AnuvaadHinTokenizer(object):
         return text
 
     def serialize_dot_with_number(self, text):
-        patterns = re.findall(r'(?:[ ][0-9]{,2}[.])',text)
+        patterns = re.findall(r'(?:[ ][0-9]{,2}[.][ ])',text)
         index = 0
         if patterns is not None and isinstance(patterns, list):
             for pattern in patterns:
