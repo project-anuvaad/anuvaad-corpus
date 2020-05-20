9155cc0cff175a75d146a37d0ede213fd44504ff
aroop <aroop@netopss-MBP-6.Dlink>
2020-05-20 15:54:38 +0530

    url modified

--- node/utils/translate_anuvaad.js
+++ node/utils/translate_anuvaad.js
@@ -1,13 +1,12 @@
 var axios = require('axios');
 var LOG = require('../logger/logger').logger
-const NMT_BASE_URL = process.env.NMT_BASE_URL ? process.env.NMT_BASE_URL : 'http://auth.anuvaad.org'
+const NMT_BASE_URL = process.env.NMT_BASE_URL ? process.env.NMT_BASE_URL : 'https://auth.anuvaad.org'
 var UUIDV4 = require('uuid/v4')
 
 
 exports.translateFromAnuvaad = function (sentences, url_end_point, cb) {
-    axios.post(NMT_BASE_URL + '/' + url_end_point ? url_end_point : 'translate-anuvaad', {
+    axios.post(NMT_BASE_URL + '/' + (url_end_point ? url_end_point : 'translate-anuvaad'),
         sentences
-    }
         , {
             headers: {
                 'Content-Type': `application/json`,
@@ -15,6 +14,7 @@ exports.translateFromAnuvaad = function (sentences, url_end_point, cb) {
             timeout: 30000,
         }
     ).then(function (api_res) {
+        LOG.info(api_res)
         if (api_res && api_res.data && api_res.data.response_body) {
             cb(null, api_res.data.response_body)
         } else {
