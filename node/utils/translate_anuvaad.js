e982380d6c750c0fe01292f4ec484f8312f29878
aroopghoshtarento <aroop.ghosh@tarento.com>
2020-05-28 17:40:21 +0530

    log removed

--- node/utils/translate_anuvaad.js
+++ node/utils/translate_anuvaad.js
@@ -5,13 +5,11 @@ var UUIDV4 = require('uuid/v4')
 
 
 exports.translateFromAnuvaad = function (sentences, url_end_point, cb) {
-    LOG.info(NMT_BASE_URL + '/' + (url_end_point ? url_end_point : 'translate-anuvaad'))
     axios.post(NMT_BASE_URL + '/' + (url_end_point ? url_end_point : 'translate-anuvaad'),
         sentences
         , {
             headers: {
                 'Content-Type': `application/json`,
-                'Authorization':'Bearer a30b0a4766fb418d863a0c2facede4e6|839ceaef0b054bb19034943975a0fb56'
             },
             timeout: 30000,
         }
